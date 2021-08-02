41b741f80eb984f8f89620a25461a13018b3e36d
Matthias Van Parijs <matvp91@gmail.com>
2021-08-02 11:40:34 +0200

    Remove unused files

--- src/LazyLoadNativeImage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function LazyLoadNativeImage({ src, width, height }) {
-  return <img loading="lazy" src={src} width={width} height={height} />;
-}
--- src/LazyLoadObserverImage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function LazyLoadObserverImage({ src, width, height }) {
-  return <img src={src} width={width} height={height} />;
-}
